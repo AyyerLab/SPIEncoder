94d1d51a4faf3f0c88aa6517d7d27029336592ec
Kartik Ayyer <kartik.ayyer@desy.de>
2021-07-05 11:29:59 +0200

    networks: Created base class with common methods

--- VAE_recon_3D/networks.py
+++ VAE_recon_3D/networks.py
@@ -1,20 +1,22 @@
+import numpy as np
+from scipy import interpolate
 import torch
 import torch.nn as nn
 
 class UnFlatten(nn.Module):
-    def forward(self, input):
-        return input.view(-1, 256, 3, 3, 3)
+    def forward(self, input_arr):
+        return input_arr.view(-1, 256, 3, 3, 3)
 
-class VAE_HD(nn.Module):
-    def __init__(self, device, image_channels=1, h_dim=128, z_dim=2, info=1):
+# Dout=(Din −1)×stride[0]−2×padding[0]+(kernel_size[0]−1)+output_padding[0]+1
+
+class BaseVAE(nn.Module):
+    def __init__(self, device, z_dim=2, info=1):
         super().__init__()
-        self.encoder = None
-        self.decoder = None
         self.device = device
         self.z_dim = z_dim
-        if (info == 0):
+        if info == 0:
             self.info_dim = 4
-        if (info > 0):
+        if info > 0:
             self.info_dim = 5
 
         self._init_encoder()
@@ -22,6 +24,61 @@ class VAE_HD(nn.Module):
         self._init_decoder()
         self.ori = None
 
+    def _init_encoder(self):
+        raise NotImplementedError('Subclass needs to implement _init_encoder')
+
+    def _init_decoder(self):
+        raise NotImplementedError('Subclass needs to implement _init_decoder')
+
+    def preproc_sample_intens(self, intens_input):
+        raise NotImplementedError('Subclass needs to implement preproc_sample_intens')
+
+    def preproc_scale_down_plane(self, input_plane):
+        raise NotImplementedError('Subclass needs to implement preproc_scale_down_plane')
+
+    def _init_latent_layers(self):
+        self.fc1 = nn.Linear(64+self.info_dim, self.z_dim)
+        self.fc2 = nn.Linear(64+self.info_dim, self.z_dim)
+        self.fc3 = nn.Linear(self.z_dim, 64)
+
+    def encode(self, x):
+        z, mu, logvar = self.latent_layer(self.encoder(x))
+        return z, mu, logvar
+
+    def decode(self, z):
+        z = self.fc3(z)
+        z = self.decoder(z)
+        return z
+
+    def latent_layer(self, h):
+        h1 = torch.cat([h, self.ori[:,:self.info_dim]], dim=1)
+        mu, logvar = self.fc1(h1), self.fc2(h1)
+        z = self.sampling(mu, logvar)#.to(device)
+        return z, mu, logvar
+
+    @staticmethod
+    def sampling(mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)
+
+    def forward(self, x_all):
+        x = x_all[0]
+        self.ori = x_all[1]
+        z, mu, logvar = self.encode(x)
+        z = self.decode(z)
+        return z, mu, logvar
+
+    @staticmethod
+    def _mask_circle(intens_input_c):
+        print('input image size = ', intens_input_c.shape)
+        imagesize = intens_input_c.shape[-1]
+        ind = np.arange(imagesize) - imagesize//2
+        x, y = np.meshgrid(ind, ind, indexing='ij')
+        intrad_2d = np.sqrt(x**2 + y**2)
+        intens_input_c[:,intrad_2d > imagesize//2] = 0.0
+
+class VAEHD(BaseVAE):
     def _init_encoder(self):
         self.encoder = nn.Sequential(
             nn.Conv2d(1, 16, kernel_size=5, stride=3, padding=1),
@@ -37,82 +94,40 @@ class VAE_HD(nn.Module):
             nn.ReLU()
             )
 
-    def _init_latent_layers(self):
-        #self.fc0 = nn.Linear(128, 64)
-        self.fc1 = nn.Linear(64+self.info_dim, self.z_dim)
-        self.fc2 = nn.Linear(64+self.info_dim, self.z_dim)
-        self.fc3 = nn.Linear(self.z_dim, 64)
-
-# Dout=(Din −1)×stride[0]−2×padding[0]+(kernel_size[0]−1)+output_padding[0]+1
     def _init_decoder(self):
         self.decoder = nn.Sequential(
-            #nn.Linear(3, 64),
             nn.Linear(64, 256*3*3*3),
             nn.ReLU(),
             UnFlatten(),
-            nn.ConvTranspose3d(256, 128, kernel_size=3, stride=3),       #Din = 3*3*3   Dout = 9*9*9
+            #Din = 3*3*3   Dout = 9*9*9
+            nn.ConvTranspose3d(256, 128, kernel_size=3, stride=3),
             nn.BatchNorm3d(128), nn.ReLU(), #nn.Dropout(p=0.1),
-            nn.ConvTranspose3d(128, 64, kernel_size=3, padding=1),      #Din = 9*9*9   Dout = 9*9*9
+            #Din = 9*9*9   Dout = 9*9*9
+            nn.ConvTranspose3d(128, 64, kernel_size=3, padding=1),
             nn.BatchNorm3d(64), nn.ReLU(), #nn.Dropout(p=0.1),
-            nn.ConvTranspose3d(64, 32, kernel_size=3, stride=3),      #Din = 9*9*9   Dout = 27*27*27
+            #Din = 9*9*9   Dout = 27*27*27
+            nn.ConvTranspose3d(64, 32, kernel_size=3, stride=3),
             nn.BatchNorm3d(32), nn.ReLU(), #nn.Dropout(p=0.1),
-            nn.ConvTranspose3d(32, 16, kernel_size=3, stride=3),      #Din = 27*27*27   Dout = 81*81*81
+            #Din = 27*27*27   Dout = 81*81*81
+            nn.ConvTranspose3d(32, 16, kernel_size=3, stride=3),
             nn.BatchNorm3d(16), nn.ReLU(), #nn.Dropout(p=0.1),
-            nn.ConvTranspose3d(16, 8, kernel_size=3, stride=3, padding=0),      #Din = 81*81*81   Dout = 243*243*243
+            #Din = 81*81*81   Dout = 243*243*243
+            nn.ConvTranspose3d(16, 8, kernel_size=3, stride=3, padding=0),
             nn.BatchNorm3d(8), nn.ReLU(), #nn.Dropout(p=0.1),
-            nn.ConvTranspose3d(8, 1, kernel_size=3, padding=1 ),      #Din = 243*243*243   Dout = 243*243*243
+            #Din = 243*243*243   Dout = 243*243*243
+            nn.ConvTranspose3d(8, 1, kernel_size=3, padding=1 ),
             nn.Sigmoid()
             )
 
-    def sampling(self, mu, logvar):
-        #std = logvar.mul(0.5).exp_().to(self.device)
-        #esp = torch.randn(*mu.size()).to(self.device)
-        #z = mu + std * esp
-        std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)
-        return eps.mul(std).add_(mu)
-        #return z
+    def preproc_sample_intens(self, intens_input):
+        intens_input_c = intens_input
+        self._mask_circle(intens_input_c)
+        return intens_input_c
 
-    def latent_layer(self, h):
-        h1 = torch.cat([h, self.ori[:,:self.info_dim]], dim=1)
-        mu, logvar = self.fc1(h1), self.fc2(h1)
-        z = self.sampling(mu, logvar)#.to(self.device)
-        return z, mu, logvar
+    def preproc_scale_down_plane(self, input_plane):
+        return input_plane / (input_plane.shape[1]//2+1)
 
-    def encode(self, x):
-        h = self.encoder(x)
-        z, mu, logvar = self.latent_layer(h)
-        return z, mu, logvar
-
-    def decode(self, z):
-        z = self.fc3(z)
-        z = self.decoder(z)
-        return z
-
-    def forward(self, x_all):
-        x = x_all[0]
-        self.ori = x_all[1]
-        z, mu, logvar = self.encode(x)
-        z = self.decode(z)
-        return z, mu, logvar
-
-class VAE_MD(nn.Module):
-    def __init__(self, device, image_channels=1, h_dim=128, z_dim=2, info=1):
-        super().__init__()
-        self.encoder = None
-        self.decoder = None
-        self.device = device
-        self.z_dim = z_dim
-        if (info == 0):
-            self.info_dim = 4
-        if (info > 0):
-            self.info_dim = 5
-
-        self._init_encoder()
-        self._init_latent_layers()
-        self._init_decoder()
-        self.ori = None
-
+class VAEMD(BaseVAE):
     def _init_encoder(self):
         self.encoder = nn.Sequential(
             nn.Conv2d(1, 16, kernel_size=5, stride=2, padding=2),
@@ -127,12 +142,6 @@ class VAE_MD(nn.Module):
             nn.Linear(3*3*128, 64),
             nn.ReLU()
             )
-
-    def _init_latent_layers(self):
-        #self.fc0 = nn.Linear(128, 64)
-        self.fc1 = nn.Linear(64+self.info_dim, self.z_dim)
-        self.fc2 = nn.Linear(64+self.info_dim, self.z_dim)
-        self.fc3 = nn.Linear(self.z_dim, 64)
 
     def _init_decoder(self):
         self.decoder = nn.Sequential(
@@ -154,54 +163,35 @@ class VAE_MD(nn.Module):
             nn.Sigmoid()
             )
 
-    def sampling(self, mu, logvar):
-        #std = logvar.mul(0.5).exp_().to(self.device)
-        #esp = torch.randn(*mu.size()).to(self.device)
-        #z = mu + std * esp
-        std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)
-        return eps.mul(std).add_(mu)
-        #return z
+    def preproc_sample_intens(self, intens_input):
+        num_l = intens_input.shape[0]
+        x_orig = np.arange(-121, 122, 1.) / 121.
+        y_orig = np.arange(-121, 122, 1.) / 121.
 
-    def latent_layer(self, h):
-        h1 = torch.cat([h, self.ori[:,:self.info_dim]], dim=1)
-        mu, logvar = self.fc1(h1), self.fc2(h1)
-        z = self.sampling(mu, logvar)#.to(self.device)
-        return z, mu, logvar
+        lmax = 80
 
-    def encode(self, x):
-        h = self.encoder(x)
-        z, mu, logvar = self.latent_layer(h)
-        return z, mu, logvar
+        x_new = np.arange(-lmax, lmax+1, 1.) / 121
+        y_new = np.arange(-lmax, lmax+1, 1.) / 121
+        intens_input_c = np.zeros([num_l, 2*lmax+1, 2*lmax+1])
+        for i in range(num_l):
+            input_map = self.intens_input0[i]
+            interpf = interpolate.interp2d(x_orig, y_orig, input_map, kind='cubic')
+            intens_input_c[i] = interpf(x_new, y_new)
 
-    def decode(self, z):
-        z = self.fc3(z)
-        z = self.decoder(z)
-        return z
+        self._mask_circle(intens_input_c)
 
-    def forward(self, x_all):
-        x = x_all[0]
-        self.ori = x_all[1]
-        z, mu, logvar = self.encode(x)
-        z = self.decode(z)
-        return z, mu, logvar
+        return intens_input_c
 
-class VAE_LD(nn.Module):
-    def __init__(self, image_channels=1, h_dim=128, z_dim=2, info=1):
-        super().__init__()
-        self.encoder = None
-        self.decoder = None
-        self.z_dim = z_dim
-        if (info == 0):
-            self.info_dim = 4
-        if (info > 0):
-            self.info_dim = 5
+    def preproc_scale_down_plane(self, input_plane):
+        x_orig = np.arange(-121, 122, 1.) / 121
+        y_orig = np.arange(-121, 122, 1.) / 121
+        interpf = interpolate.interp2d(x_orig, y_orig, input_plane, kind='cubic')
 
-        self._init_encoder()
-        self._init_latent_layers()
-        self._init_decoder()
-        self.ori = None
+        x_new = np.arange(-80, 81, 1.) / 121.
+        y_new = np.arange(-80, 81, 1.) / 121.
+        return interpf(x_new, y_new)/(len(x_new)//2+1)  #MD
 
+class VAELD(BaseVAE):
     def _init_encoder(self):
         self.encoder = nn.Sequential(
             nn.Conv2d(1, 16, kernel_size=3, stride=3, padding=1),
@@ -214,12 +204,6 @@ class VAE_LD(nn.Module):
             nn.Linear(3*3*128, 64),
             nn.ReLU()
             )
-
-    def _init_latent_layers(self):
-        #self.fc0 = nn.Linear(128, 64)
-        self.fc1 = nn.Linear(64+self.info_dim, self.z_dim)
-        self.fc2 = nn.Linear(64+self.info_dim, self.z_dim)
-        self.fc3 = nn.Linear(self.z_dim, 64)
 
     def _init_decoder(self):
         self.decoder = nn.Sequential(
@@ -239,32 +223,45 @@ class VAE_LD(nn.Module):
             nn.Sigmoid()
             )
 
-    def sampling(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)
-        return eps.mul(std).add_(mu)
+    def preproc_sample_intens(self, intens_input):
+        num_l = intens_input.shape[0]
+        x_orig = np.arange(-121, 122, 1.) / 121.
+        y_orig = np.arange(-121, 122, 1.) / 121.
 
-    def latent_layer(self, h):
-        h1 = torch.cat([h, self.ori[:,:self.info_dim]], dim=1)
-        mu, logvar = self.fc1(h1), self.fc2(h1)
-        z = self.sampling(mu, logvar)#.to(device)
-        return z, mu, logvar
+        lmax = 40
 
-    def encode(self, x):
-        h = self.encoder(x)
-        z, mu, logvar = self.latent_layer(h)
-        return z, mu, logvar
+        x_new = np.arange(-lmax, lmax+1, 1.) / 121
+        y_new = np.arange(-lmax, lmax+1, 1.) / 121
+        intens_input_c = np.zeros([num_l, 2*lmax+1, 2*lmax+1])
+        for i in range(num_l):
+            input_map = self.intens_input0[i]
+            interpf = interpolate.interp2d(x_orig, y_orig, input_map, kind='cubic')
+            intens_input_c[i] = interpf(x_new,y_new)
 
-    def decode(self, z):
-        z = self.fc3(z)
-        z = self.decoder(z)
-        return z
+        self._mask_circle(intens_input_c)
 
-    def forward(self, x_all):
-        x = x_all[0]
-        self.ori = x_all[1]
-        z, mu, logvar = self.encode(x)
-        z = self.decode(z)
-        return z, mu, logvar
+        return intens_input_c
 
-VAE_LD_Paper = VAE_LD
+    def preproc_scale_down_plane(self, input_plane):
+        x_orig = np.arange(-121, 122, 1.) / 121
+        y_orig = np.arange(-121, 122, 1.) / 121
+        interpf = interpolate.interp2d(x_orig, y_orig, input_plane, kind='cubic')
+
+        x_new = np.arange(-40, 41, 1.) / 121.
+        y_new = np.arange(-40, 41, 1.) / 121.
+        return interpf(x_new, y_new)/(len(x_new)//2+1)  #MD
+
+class VAELDPaper(VAELD):
+    def preproc_sample_intens(self, intens_input):
+        intens_input_c = intens_input[:,42:204:2,42:204:2]
+        self._mask_circle(intens_input_c)
+        return intens_input_c
+
+    def preproc_scale_down_plane(self, input_plane):
+        x_orig = np.arange(-121, 122, 1.) / 121
+        y_orig = np.arange(-121, 122, 1.) / 121
+        interpf = interpolate.interp2d(x_orig, y_orig, input_plane, kind='cubic')
+
+        x_new = np.arange(-40, 41, 1) / 60.
+        y_new = np.arange(-40, 41, 1) / 60.
+        return interpf(x_new, y_new) / (len(x_new)*2//2+1)
